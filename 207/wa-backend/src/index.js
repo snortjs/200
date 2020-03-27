ba1a3beecf7b1fc31dc4cf2e5e98f8897a0b0b8f
snortjs <35138382+snortjs@users.noreply.github.com>
2020-03-27 15:14:56 +0100

    Update index.js

--- 207/wa-backend/src/index.js
+++ 207/wa-backend/src/index.js
@@ -1,33 +1,36 @@
 import express from 'express';
-import storage from './memory_storage.js'
+import storage from './memory_storage'
 import cors from 'cors'
 
-const app = express()  // instanciranje aplikacije
-const port = 3000  // port na kojem će web server slušati
-
+const app = express() // instanciranje aplikacije
+const port = 3000 // port na kojem će web server slušati
 app.use(cors())
+app.get('/posts',(req,res)=>{
 
-app.get('/posts', (req, res) => {
-    let posts = storage.posts
-    let query = req.query
-    
-    if (query.title) {
-        posts = posts.filter(e => e.title.indexOf(query.title) >= 0)
-    }
-    
-    if (query.createdBy) {
-        posts = posts.filter(e => e.createdBy.indexOf(query.createdBy) >= 0)
-    }
-    
-    if (query._any) {
-        let terms = query._any.split(" ")
-        posts = posts.filter(doc => {
-            let info = doc.title + " " + doc.createdBy
-            return terms.every(term => info.indexOf(term) >= 0)
+    let postovi=storage.posts
+
+    let query=req.query
+    if(query.title){
+        postovi=postovi.filter(e=>{
+            e.title.indexOf(query.title)>=0
         })
     }
-
-    res.json(posts)
+    if(query.createdBy){
+        postovi=postovi.filter(a=>{
+            a.createdBy.indexOf(query.createdBy)>=0
+        })
+    }
+    if(query._any){
+        let pretraga=query._any
+        let pojmovi=pretraga.split(" ")
+        postovi=postovi.filter(post=>{
+            let podaci=post.title+post.createdBy
+            let rezultat=pojmovi.every(pojam=>{
+                return podaci.indexOf(pojam)>=0
+            })
+            return rezultat
+        })
+    }
+    res.json(postovi)
 })
-
 app.listen(port, () => console.log(`Slušam na portu ${port}!`))
